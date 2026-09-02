27de611fe4da652bdb194b38c39c0de0327d0f37
vboxuser <vboxuser@dev.myguest.virtualbox.org>
2026-09-02 10:55:41 +0200

    test

--- data_visualization/scatter_plot.py
+++ data_visualization/scatter_plot.py
@@ -61,7 +61,7 @@ def main(argc: int, argv: list[str]):
     plt.ylabel(course_b)
     plt.title(f"{course_a} vs {course_b} (r = {best_correlation:.4f})")
     plt.tight_layout()
-    plt.show()
+    plt.savefig("scatter_plot.png")
 
 if __name__ == "__main__":
     main(len(sys.argv), sys.argv)
