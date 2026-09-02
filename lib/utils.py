521df332b236ed2b843b1e2b2fb21cffb89e1d8e
Antoineloisea1 <antoineloisea1@gmail.com>
2026-09-02 12:01:06 +0200

    histo display most homogeneous course

--- lib/utils.py
+++ lib/utils.py
@@ -1,12 +1,5 @@
 import math
 
-HOUSE_COLORS = {
-    "Gryffindor": "#AE0001",
-    "Slytherin": "#2A623D",
-    "Ravenclaw": "#222F5B",
-    "Hufflepuff": "#ECB939",
-}
-
 def to_float(value: str):
     if not value:
         return None
